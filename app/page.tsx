7e6b53b22685a8edfbcf73fb9e77760122b6fb4e
mariosknl <mariosknl@outlook.com>
2022-12-27 10:09:02 +0200

    building the article component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+import response from "../response.json";
 
 async function Homepage() {
 	// fetch news data
-	const news: NewsResponse = await fetchNews(categories.join(","));
+	const news: NewsResponse =
+		response || (await fetchNews(categories.join(",")));
 
-	console.log(news);
-
-	return <div>{/* NewList news */}</div>;
+	return (
+		<div>
+			<NewsList news={news} />
+		</div>
+	);
 }
 
 export default Homepage;
