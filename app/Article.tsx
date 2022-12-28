3e97e29d6f103eb3a88f049ec3a05309c8a0564a
mariosknl <mariosknl@outlook.com>
2022-12-28 09:21:52 +0200

    LiveStamp component created

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,3 +1,4 @@
+import LiveTimestamp from "./LiveTimestamp";
 import ReadMoreButton from "./ReadMoreButton";
 
 type Props = {
@@ -6,7 +7,7 @@ type Props = {
 
 function Article({ article }: Props) {
 	return (
-		<article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
+		<article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
 			{article.image && (
 				<img
 					src={article.image}
@@ -19,17 +20,19 @@ function Article({ article }: Props) {
 				<div className="flex-1 flex flex-col p-5">
 					<h2 className="font-bold font-serif">{article.title}</h2>
 
-					<section className="flex-1 mt-2">
-						<p className="text-xs line-clamp-6">{article.description}</p>
+					<section className="mt-2 flex-1">
+						<p className="text-xs line-clamp-2">{article.description}</p>
 					</section>
 
 					<footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
-						<p>{article.source} - </p>
-						<p>{article.published_at}</p>
+						<p>{article.source} -</p>
+						<p>
+							<LiveTimestamp time={article.published_at} />
+						</p>
 					</footer>
 				</div>
 
-				{/* readmore button */}
+				{/* ReadMoreButton */}
 				<ReadMoreButton article={article} />
 			</div>
 		</article>
--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,4 +1,5 @@
 import { notFound } from "next/navigation";
+import LiveTimestamp from "../LiveTimestamp";
 
 type Props = {
 	searchParams?: Article;
@@ -19,24 +20,24 @@ function ArticlePage({ searchParams }: Props) {
 			<section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
 				{article.image && (
 					<img
+						className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
 						src={article.image}
 						alt={article.title}
-						className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
 					/>
 				)}
 
-				<div>
+				<div className="px-10">
 					<h1 className="headerTitle px-0 no-underline pb-2">
 						{article.title}
 					</h1>
-				</div>
 
-				<div className="flex divide-x-2 space-x-4">
-					<h2 className="font-bold">By: {article.author || "unknown"}</h2>
-					<h2 className="font-bold pl-4">
-						Source: {article.source || "unknown"}
-					</h2>
-					<p className="pl-4">{article.published_at}</p>
+					<div className="flex divide-x-2 space-x-4">
+						<h2 className="font-bold">By: {article.author}</h2>
+						<h2 className="font-bold pl-4">Source: {article.source}</h2>
+						<p className="pl-4">
+							<LiveTimestamp time={article.published_at} />
+						</p>
+					</div>
 
 					<p className="pt-4">{article.description}</p>
 				</div>
