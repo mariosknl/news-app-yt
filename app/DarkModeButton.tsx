57036cfbbd2dace21d258d544914f55c303351e6
mariosknl <mariosknl@outlook.com>
2022-12-28 08:42:25 +0200

    dark mode enabled

---
+++ app/DarkModeButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+
+function DarkModeButton() {
+	const [mounted, setMounted] = useState(false);
+	const { theme, setTheme, systemTheme } = useTheme();
+
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) return null;
+
+	const currentTheme = theme === "system" ? systemTheme : theme;
+
+	return (
+		<div>
+			{currentTheme === "dark" ? (
+				<SunIcon
+					className="h-8 w-8 cursor-pointer text-yellow-500"
+					onClick={() => setTheme("light")}
+				/>
+			) : (
+				<MoonIcon
+					className="h-8 w-8 cursor-pointer text-gray-900"
+					onClick={() => setTheme("dark")}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default DarkModeButton;
