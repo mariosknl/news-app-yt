906f1945bdbf4a8b178b0705ec53836ed4182cc6
mariosknl <mariosknl@outlook.com>
2022-12-28 09:13:47 +0200

    implementing dynamic routing nextjs 13

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
 import NewsList from "./NewsList";
+import response from "../response.json";
 
 async function Homepage() {
-	const news: NewsResponse = await fetchNews(categories.join(","));
+	// fetch news data
+	const news: NewsResponse =
+		response || (await fetchNews(categories.join(",")));
 
 	return (
 		<div>
