4b66409477d376e92b98e8d6741564897aabd255
mariosknl <mariosknl@outlook.com>
2022-12-28 09:05:44 +0200

    search Page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import { categories } from "../constants";
 import fetchNews from "../lib/fetchNews";
 import NewsList from "./NewsList";
-import response from "../response.json";
 
 async function Homepage() {
-	// fetch news data
-	const news: NewsResponse =
-		response || (await fetchNews(categories.join(",")));
+	const news: NewsResponse = await fetchNews(categories.join(","));
 
 	return (
 		<div>
